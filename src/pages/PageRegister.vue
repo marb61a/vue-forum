833102dc12473b79d59e6d28f12d488c0aa485ce
marb61a <marb61a@gmail.com>
2018-10-11 16:55:50 +0100

    updated register page form

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -6,11 +6,27 @@
         <div class="form-group">
           <label for="name">Full Name</label>
           <input
-
+            v-model="form.name"
+            @blur="$v.form.name.$touch()"
+            id="name"
+            type="text"
+            class="form-input"
           >
-          <template>
-            <span></span>
+          <template v-if="$v.form.name.$error">
+            <span v-if="!$v.form.name.required" class="form-error">
+              This is a required field
+            </span>
           </template>
+        </div>
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input
+            v-model.lazy="form.username"
+            @blur="$v.form.username.$touch()"
+            id="username"
+            type="text"
+            class="form-input"
+          >
         </div>
         <div class="form-actions">
           <button type="submit" class="btn-blue btn-block">
@@ -48,11 +64,27 @@
         username: {
           required,
           unique: uniqueUsername
+        },
+        email: {
+          required,
+          email,
+          unique: uniqueEmail
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        },
+        avatar: {
+          url,
+          supportedImageFile,
+          responseOk
         }
       }
     },
     methods: {
+      register () {
 
+      }
     }
   }
 </script>
