3a586f12183a79fd39696552a3756f5f18f18de7
marb61a <marb61a@gmail.com>
2018-10-11 12:44:19 +0100

    updated register page

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -4,11 +4,24 @@
       <form @submit.prevent="register" class="card card-form">
         <h1 class="text-center">Register</h1>
         <div class="form-group">
+          <label for="name">Full Name</label>
+          <input
 
+          >
+          <template>
+            <span></span>
+          </template>
+        </div>
+        <div class="form-actions">
+          <button type="submit" class="btn-blue btn-block">
+            Register
+          </button>
         </div>
       </form>
       <div text-center push-top>
+        <button>
 
+        </button>
       </div>
     </div>
   </div>
@@ -19,9 +32,27 @@
     data () {
       return {
         form: {
-
+          name: null,
+          username: null,
+          email: null,
+          password: null,
+          avatar: null
         }
       }
+    },
+    validations: {
+      form: {
+        name: {
+          required
+        },
+        username: {
+          required,
+          unique: uniqueUsername
+        }
+      }
+    },
+    methods: {
+
     }
   }
 </script>
