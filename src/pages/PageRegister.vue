f614f390607a405ff4c345420ea5eab91db076c4
marb61a <marb61a@gmail.com>
2018-10-11 12:37:47 +0100

    added initial register page

---
+++ src/pages/PageRegister.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex-grid justify-center">
+    <div class="col-2">
+      <form @submit.prevent="register" class="card card-form">
+        <h1 class="text-center">Register</h1>
+        <div class="form-group">
+
+        </div>
+      </form>
+      <div text-center push-top>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        form: {
+
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
