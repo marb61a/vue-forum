f8e76dc63aa0f56af4c3f904f1a4dade9ac5dd9f
marb61a <marb61a@gmail.com>
2018-10-14 11:51:09 +0100

    added image upload validation and stling

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -11,6 +11,28 @@ export const uniqueUsername = (value) => {
   })
 }
 
+export const supportedImageFile = (value) => {
+  if (!vuelidateHelpers.req(value)) {
+    return true
+  }
+
+  const supported = ['jpg', 'jpeg', 'gif', 'png', 'svg']
+  const suffix = value.split('.').pop()
+  return supported.includes(suffix)
+}
+
+export const responseOk = (value) => {
+  if (!vuelidateHelpers.req(value)) {
+    return true
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(value)
+      .then(response => resolve(response.ok))
+      .catch(() => resolve(false))
+  })
+}
+
 export const uniqueEmail = (value) => {
   if (!vuelidateHelpers.req(value)) {
     return true
