f2995cbea13d899188547911e732f05426f3b661
marb61a <marb61a@gmail.com>
2018-10-14 12:01:17 +0100

    added basic navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header" id="header">
+    <router-link
+      :to="{name: 'Home'}"
+      class="logo"
+    >
+      <img src="">
+    </router-link>
+    <div class="btn-hamburger">
+      <div class="top bar"></div>
+      <div class="middle bar"></div>
+      <div class="bottom bar"></div>
+    </div>
+    <nav class="navbar">
+      <ul v-if="user">
+        <li class="navbar-user">
+          <a @click.prevent="userDropdownOpen = !userDropdownOpen">
+            <img class="avatar-small" :src="user.avatar" alt="">
+            <span>
+              {{user.name}}
+            </span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    data () {
+
+    }
+  }
+</script>
+
+<style scoped>
+</style>
