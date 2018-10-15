aada2f95d8b87efd7ca897e671fbd375381b0202
marb61a <marb61a@gmail.com>
2018-10-15 17:06:00 +0100

    updated PageForum page and added mixin

--- src/pages/PageForum.vue
+++ src/pages/PageForum.vue
@@ -1,7 +1,17 @@
 <template>
   <div v-if="asyncDataStatus_ready" class="forum-wrapper">
     <div class="col-full push-top">
-
+      <div class="forum-header">
+        <div class="forum-details">
+          <h1>{{forum.name}}</h1>
+          <p class="text-lead">{{forum.description}}</p>
+        </div>
+        <router-link
+          class="btn-green btn-small"
+        >
+          Start a thread
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -9,6 +19,7 @@
 <script>
 import {mapActions} from 'vuex'
 import ThreadList from '@/components/ThreadList'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 
 export default {
   components: {
@@ -22,6 +33,12 @@ export default {
     }
   },
   computed: {
+
+  },
+  methods: {
+
+  },
+  created () {
 
   }
 }
