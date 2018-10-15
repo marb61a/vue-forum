9f8a70fb739117a334f702c3fff68cc00256b19b
marb61a <marb61a@gmail.com>
2018-10-15 15:34:02 +0100

    added forum list item component

---
+++ src/components/ForumListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="forum-listing">
+    <div class="forum-details">
+      <router-link>
+        {forum.name}
+      </router-link>
+      <p>{{forum.description}}</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import {countObjectProperties} from '@/utils'
+
+export default {
+  props: {
+    forum: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
