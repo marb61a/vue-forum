fe86a923dc8d1309be35115eed52be754b670bc9
marb61a <marb61a@gmail.com>
2018-10-15 16:57:45 +0100

    added initial PageForum page

---
+++ src/pages/PageForum.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="asyncDataStatus_ready" class="forum-wrapper">
+    <div class="col-full push-top">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+import ThreadList from '@/components/ThreadList'
+
+export default {
+  components: {
+    ThreadList
+  },
+  mixins: [],
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .forum-wrapper {
+    width: 100%;
+  }
+</style>
