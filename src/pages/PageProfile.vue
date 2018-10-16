13412407ba5818e026fcd8d538460d365ae2bc99
marb61a <marb61a@gmail.com>
2018-10-16 12:27:16 +0100

    Added initial PageProfile page

---
+++ src/pages/PageProfile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex-grid">
+    <div class="col-7 push-top">
+      <div class="profile-header">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import asyncDataStatus from '@/mixins/asyncDataStatus'
+
+export default {
+  components:{
+
+  },
+  mixins: [asyncDataStatus],
+  props: {
+    edit: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
