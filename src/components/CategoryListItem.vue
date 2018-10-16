bda82afb4a2d7c9d9f3d42fd47178aec9b7e3aac
marb61a <marb61a@gmail.com>
2018-10-16 16:54:49 +0100

    added category list item component

---
+++ src/components/CategoryListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="forum-list">
+    <h2 class="list-title">
+      <router-link>
+
+      </router-link>
+    </h2>
+    <ForumList />
+  </div>
+</template>
+
+<script>
+import ForumList from './ForumList'
+
+export default {
+  components: {
+    ForumList
+  },
+  props: {
+    category: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
