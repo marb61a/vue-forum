588cba4586ab753ec60385b15a2f9dc43a23e5a4
marb61a <marb61a@gmail.com>
2018-10-17 16:39:52 +0100

    added thread show page

---
+++ src/pages/PageThreadShow.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>
+      {{thread.title}}
+      <router-link>
+
+      </router-link>
+    </h1>
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
+import {countObjectProperties} from '@/utils'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
+
+export default {
+  components: {
+    PostList,
+    PostEditor
+  },
+  mixins: [asyncDataStatus],
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  created () {
+
+  }
+}
+</script>
