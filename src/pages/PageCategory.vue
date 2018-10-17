e099ba537b4bbeeb421aa8e284b5ba8307f26ad8
marb61a <marb61a@gmail.com>
2018-10-17 16:45:11 +0100

    added category page

---
+++ src/pages/PageCategory.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="asyncDataStatus_ready" class="col-full">
+    {{category.name}}
+    <CategoryListItem :category="category"/>
+  </div>
+</template>
+
+<script>
+import CategoryListItem from '@/components/CategoryListItem'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
+
+export default {
+  components: {
+    CategoryListItem
+  },
+  mixins: [asyncDataStatus],
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+  computed: {
+    category () {
+
+    }
+  },
+  methods: {
+
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
