3a1b202ea71e38bb89af028c583d5921e1f2e889
marb61a <marb61a@gmail.com>
2018-10-17 11:43:50 +0100

    added thread list item component

---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="thread && user" class="thread">
+    <div>
+      <p>
+        <router-link>
+          {{thread.title}}
+        </router-link>
+      </p>
+      <p class="text-faded text-xsmall">
+
+      </p>
+    </div>
+    <div class="activity">
+      <p class="replies-count">
+
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    thread: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
