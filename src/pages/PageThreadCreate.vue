f1d772b8ad04adf087573a8eebcc2e94d1ca7c6e
marb61a <marb61a@gmail.com>
2018-10-17 12:38:06 +0100

    added initial thread create page

---
+++ src/pages/PageThreadCreate.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>
+      Create a new thread
+    </h1>
+    <ThreadEditor />
+  </div>
+</template>
+
+<script>
+import ThreadEditor from '@/components/ThreadEditor'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
+
+export default {
+  components: {
+    ThreadEditor
+  },
+  mixins: [asyncDataStatus],
+  props: {
+    forumId: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      saved: false
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
