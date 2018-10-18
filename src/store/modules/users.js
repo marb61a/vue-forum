d3410e6bde5c2ee8c49d0cad57ecb6f8dde7d348
marb61a <marb61a@gmail.com>
2018-10-18 16:54:48 +0100

    updated usres store module and added assetHelpers store file

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -20,11 +20,27 @@ export default {
   actions: {
     createUser ({state, commit}, {id, email, name, username, avatar = null}) {
       return new Promise((resolve, reject) => {
+        const registeredAt = Math.floor(Date.now() / 1000)
+        const usernameLower = username.toLowerCase()
+        email = email.toLowerCase()
+        const user = {
+          avatar, email, name, username, usernameLower, registeredAt
+        }
 
+        firebase.database().ref('users')
+          .child(id)
+          .set(user)
+          .then()
       })
     }
   },
   mutations: {
-
+    setUser (state, {user, userId}) {
+      Vue.set(state.items, userId, user)
+    },
+    appendThreadToUser: makeAppendChildToParentMutation({
+      parent: 'users',
+      child: 'threads'
+    })
   }
 }
