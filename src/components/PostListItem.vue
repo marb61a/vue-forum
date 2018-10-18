3afc0560e344c1ac785f31d9622083dda4a655b5
marb61a <marb61a@gmail.com>
2018-10-18 11:36:28 +0100

    added initial PostListItem component

---
+++ src/components/PostListItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="post && user" class="post">
+    <div class="user-info">
+      <a href="#">
+        <img class="avatar-large" :src="user.avatar" alt="">
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostEditor from './PostEditor'
+
+export default {
+  props: {
+    post: {
+      required: true,
+      type: Object
+    }
+  },
+  components: {
+    PostEditor
+  },
+  data () {
+    return {
+      editing: false
+    }
+  },
+  computed: {
+    user () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
