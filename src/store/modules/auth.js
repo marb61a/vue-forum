4efbaa5bf325280e8844ca9c7e74d675419ad26f
marb61a <marb61a@gmail.com>
2018-10-18 12:25:20 +0100

    added initial store files

---
+++ src/store/modules/auth.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+export default {
+  namespaced: true,
+  state: {
+    authId: null,
+    unsubscribeAuthObserver: null
+  },
+  getters: {
+
+  },
+  actions: {
+
+  },
+  mutations: {
+
+  }
+}
