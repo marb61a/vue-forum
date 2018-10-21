3325a3b43e07be02bd0f7a7eedc60e7b3b008586
marb61a <marb61a@gmail.com>
2018-10-21 11:43:45 +0100

    added initial PostEditor component

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-actions">
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        class="form-input"
+        v-model="text"
+      >
+      </textarea>
+    </div>
+    <div class="form-group">
+
+    </div>
+  </form>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+  props: {
+    threadId: {
+      required: false
+    },
+    post: {
+      type: Object
+    }
+  },
+  data () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
