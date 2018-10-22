fd4f03c7b9f7145538083e5a5d89c46104b2d98f
marb61a <marb61a@gmail.com>
2018-10-22 17:13:07 +0100

    added initial ThreadEditor component

---
+++ src/components/ThreadEditor.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input
+        v-model="form.title"
+        @blur="$v.form.title.$touch()"
+        type="text"
+        id="thread_title"
+        class="form-input"
+        name="title"
+      >
+      <template>
+
+      </template>
+    </div>
+    <div class="form-group">
+
+    </div>
+  </form>
+</template>
+
+<script>
+import { required, minLength } from 'vuelidate/lib/validators'
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
