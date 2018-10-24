e51910bdbc3ab2a0b865e27630b81b0cbdb2ec34
marb61a <marb61a@gmail.com>
2018-10-24 16:56:01 +0100

    updated PageThreadShow page

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import {mapActions, mapGetters} from 'vuex'
 import PostList from '@/components/PostList'
 import PostEditor from '@/components/PostEditor'
 import {countObjectProperties} from '@/utils'
@@ -28,13 +29,43 @@ export default {
     }
   },
   computed: {
-
+    ...mapGetters({
+      authUser: 'auth/authUser'
+    }),
+    thread () {
+      return this.$store.state.threads.items[this.id]
+    },
+    repliesCount () {
+      return this.$store.getters['threads/threadRepliesCount'](this.thread['.key'])
+    },
+    user () {
+      return this.$store.state.users.items[this.thread.userId]
+    },
+    contributorsCount () {
+      return countObjectProperties(this.thread.contributors)
+    },
+    posts () {
+      const postIds = Object.values(this.thread.posts)
+      return Object.values(this.$store.state.posts.items)
+        .filter(post => postIds.includes(post['.key']))
+    }
   },
   methods: {
-
+    ...mapActions('threads', ['fetchThread']),
+    ...mapActions('users', ['fetchUser']),
+    ...mapActions('posts', ['fetchPosts'])
   },
   created () {
+    // Fetch the thread
+    this.fetchThread({id: this.id})
+      .then(thread => {
+        // Fetch user
 
+      })
+      .then(posts => {
+
+      })
+      .then(() => { this.asyncDataStatus_fetched() })
   }
 }
 </script>
