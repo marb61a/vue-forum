a873137fa1b6ebc611299a9fd4acd6c6e752602b
marb61a <marb61a@gmail.com>
2018-10-25 11:32:23 +0100

    updated threads store module

--- src/store/modules/threads.js
+++ src/store/modules/threads.js
@@ -9,16 +9,39 @@ export default {
     items: {}
   },
   getters: {
-
+    threadRepliesCount: state => id =>
+      countObjectProperties(state.items[id].posts) - 1
   },
   actions: {
-    createThread () {
+    createThread ({state, commit, dispatch, rootState}, {text, title, forumId}) {
+      return new Promise((resolve, reject) => {
+        const threadId = firebase.database().ref('threads').push().key
+        const postId = firebase.database().ref('posts').push().key
+        const userId = rootState.auth.authId
+        const publishedAt = Math.floor(Date.now() / 1000)
 
+        const thread = {
+          title, forumId, publishedAt, userId, firstPostId: postId, posts: {}
+        }
+        thread.posts[postId] = postId
+        const post = {text, publishedAt, threadId, userId}
+      })
+    },
+    updateThread ({state, commit, dispatch, rootState}, {title, text, id}) {
+      return new Promise((resolve, reject) => {
+
+      })
     }
   },
   mutations: {
     setThread (state, {thread, threadId}) {
       Vue.set(state.items, threadId, thread)
-    }
+    },
+    appendPostToThread: makeAppendChildToParentMutation({
+      parent: 'threads', child: 'posts'
+    }),
+    appendContributorToThread: makeAppendChildToParentMutation({
+      parent: 'threads', child: 'contributors'
+    })
   }
 }
