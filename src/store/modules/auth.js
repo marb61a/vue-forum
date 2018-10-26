f20e6df610297315e488e8f8176675505fa31d79
marb61a <marb61a@gmail.com>
2018-10-26 11:45:54 +0100

    updated auth module actions

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -7,10 +7,38 @@ export default {
     unsubscribeAuthObserver: null
   },
   getters: {
-
+    authUser (state, getters, rootState) {
+      return state.authId ? rootState.users.items[state.authId] : null
+    }
   },
   actions: {
+    initAuthentication ({dispatch, commit, state}) {
+      return new Promise((resolve, reject) => {
+        // If already listening unsubscribe observer
+        if (state.unsubscribeAuthObserver) {
+          state.unsubscribeAuthObserver()
+        }
 
+        const unsubscribe = firebase.auth()
+          .onAuthStateChanged(user => {
+            console.log('User has changed')
+            if (user) {
+              dispatch('fetchAuthUser')
+                .then(dbUser => resolve(dbUser))
+            } else {
+              resolve(null)
+            }
+          })
+
+        commit('setUnsubscribeAuthObserver', unsubscribe)
+      })
+    },
+    registerUserWithEmailAndPassword () {
+
+    },
+    signInWithEmailAndPassword () {
+
+    }
   },
   mutations: {
 
