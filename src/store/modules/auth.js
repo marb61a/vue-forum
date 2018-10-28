b279814d54ddfb5ed429e0c84152a25d62a505c2
marb61a <marb61a@gmail.com>
2018-10-28 11:54:31 +0000

    updated auth store module actions

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -45,11 +45,30 @@ export default {
         })
         .then(() => dispatch('fetchAuthUser'))
     },
-    signInWithEmailAndPassword () {
-
+    signInWithEmailAndPassword (context, {email, password}) {
+      return firebase.auth()
+        .signInWithEmailAndPassword(email, password)
     },
     signInWithGoogle ({dispatch}) {
+      const provider = new firebase.auth.GoogleAuthProvider()
 
+      return firebase.auth().signInWithPopup(provider)
+        .then(data => {
+          const user = data.user
+          firebase.database().ref('users').child(user.uid)
+            .once('value', snapshot => {
+              if (!snapshot.exists()) {
+                return dispatch('users/createUser', {
+                  id: user.uid,
+                  name: user.displayName,
+                  email: user.email,
+                  username: user.email,
+                  avatar: user.photoURL
+                }, {root: true})
+                  .then(() => dispatch('fetchAuthUser'))
+              }
+            })
+        })
     },
     signOut ({commit}) {
       return firebase.auth().signOut()
@@ -58,7 +77,12 @@ export default {
         })
     },
     fetchAuthUser ({dispatch, commit}) {
+      const userId = firebase.auth().currentUser.uid
 
+      return new Promise((resolve, reject) => {
+        // Check if user exists in DB
+
+      })
     }
   },
   mutations: {
