7c053484d26bda85c857c186ff1600273f7f50cb
marb61a <marb61a@gmail.com>
2018-10-29 16:44:21 +0000

    Added initial ThreadEdit page

---
+++ src/pages/PageThreadEdit.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-if="asyncDataStatus_ready" class="col-full push-top">
+    <h1>Editing <i>{{thread.title}}</i></h1>
+    <ThreadEditor
+
+    >
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+import ThreadEditor from '@/components/ThreadEditor'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
+
+export default {
+  components: {
+    ThreadEditor
+  },
+  mixins: [asyncDataStatus],
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    thread () {
+      return this.$store.state.thread.items[this.id]
+    },
+    text () {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
