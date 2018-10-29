41030547deef1587dfb67d0d655eb4ea951ea80d
marb61a <marb61a@gmail.com>
2018-10-29 11:56:37 +0000

    Updated PostListItem template

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -4,6 +4,34 @@
       <a href="#">
         <img class="avatar-large" :src="user.avatar" alt="">
       </a>
+      <p class="desktop-only text-small">
+        {{userThreadsCount}} threads
+      </p>
+      <p class="desktop-only text-small">
+        {{userPostsCount}} posts
+      </p>
+    </div>
+    <div class="post-content">
+      <template v-if="!editing">
+        <div>
+          {{post.text}}
+        </div>
+        <a
+          @click.prevent="editing = true"
+          href="#"
+          style="margin-left: auto;"
+          class="link-unstyled" title="Make a change"
+        >
+          <i class="fa fa-pencil"></i>
+        </a>
+      </template>
+      <div v-else>
+        <PostEditor
+          :post="post"
+          @save="editing = false"
+          @cancel="editing = false"
+        >
+      </div>
     </div>
   </div>
 </template>
